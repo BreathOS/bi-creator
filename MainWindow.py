05162c234da9bff4ca8912fb50b1d6f043527e1a
Tolstosheyev Roman <izobretatel777@mail.ru>
2021-08-21 14:25:36 +0400

    replace usage of .ui files with .py ones

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,9 @@
 from functools import partial
 
-from PyQt6 import uic, QtCore
+from forms.mainwindow import Ui_MainMenu
+from forms.loadingscreen import Ui_LoadScreenWindow
+
+from PyQt6 import QtCore
 from PyQt6.QtWidgets import QMainWindow, QLabel, QPushButton, QFileDialog, QMessageBox, QWidget
 from PyQt6.QtCore import QThread, Qt, pyqtSlot
 
@@ -16,27 +19,27 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.msg = LoadingScreen()
-        uic.loadUi('forms/mainwindow.ui', self)
-        self.show()
+        self.ui = Ui_MainMenu()
+        self.ui.setupUi(self)
         self.packageElement = None
         self.biPackage = None
         self.creator = None
-        self.addButton.clicked.connect(self.addElement)
-        self.createPackageButton.clicked.connect(self.createPackage)
-        self.saveChangesButton.clicked.connect(self.saveChanges)
-        self.cancelButton.clicked.connect(self.cancel)
-        self.elementType.activated.connect(self.typeSelected)
+        self.ui.addButton.clicked.connect(self.addElement)
+        self.ui.createPackageButton.clicked.connect(self.createPackage)
+        self.ui.saveChangesButton.clicked.connect(self.saveChanges)
+        self.ui.cancelButton.clicked.connect(self.cancel)
+        self.ui.elementType.activated.connect(self.typeSelected)
         self.worker = QThread()
 
     def typeSelected(self):
-        curOption = self.elementType.currentText()
+        curOption = self.ui.elementType.currentText()
         if curOption == 'Select type':
-            self.addButton.setEnabled(False)
+            self.ui.addButton.setEnabled(False)
         else:
-            self.addButton.setEnabled(True)
+            self.ui.addButton.setEnabled(True)
 
     def addElement(self):
-        curOption = self.elementType.currentText()
+        curOption = self.ui.elementType.currentText()
         if curOption == 'File':
             self.packageElement = FileController()
         if curOption == 'Folder':
@@ -48,16 +51,16 @@ class MainWindow(QMainWindow):
 
     def createPackage(self):
         self.biPackage = BiPackage()
-        self.elementType.setEnabled(True)
-        self.saveChangesButton.setEnabled(True)
-        self.cancelButton.setEnabled(True)
-        self.createPackageButton.setEnabled(False)
-        self.biPackageName.setEnabled(True)
-        self.biPackageVersion.setEnabled(True)
+        self.ui.elementType.setEnabled(True)
+        self.ui.saveChangesButton.setEnabled(True)
+        self.ui.cancelButton.setEnabled(True)
+        self.ui.createPackageButton.setEnabled(False)
+        self.ui.biPackageName.setEnabled(True)
+        self.ui.biPackageVersion.setEnabled(True)
 
     def saveChanges(self):
-        name = self.biPackageName.text()
-        version = self.biPackageVersion.text()
+        name = self.ui.biPackageName.text()
+        version = self.ui.biPackageVersion.text()
         if len(self.biPackage.getElements()) == 0:
             QMessageBox().critical(self, "Error", "Suitcase cannot be empty")
         elif not name or name.isspace():
@@ -81,39 +84,40 @@ class MainWindow(QMainWindow):
                 self.worker.start()
 
     def cancel(self):
-        self.elementType.setEnabled(False)
-        self.addButton.setEnabled(False)
-        self.saveChangesButton.setEnabled(False)
-        self.cancelButton.setEnabled(False)
-        self.createPackageButton.setEnabled(True)
-        self.biPackageName.setEnabled(False)
-        self.biPackageVersion.setEnabled(False)
-        self.elementType.setCurrentIndex(0)
+        self.ui.elementType.setEnabled(False)
+        self.ui.addButton.setEnabled(False)
+        self.ui.saveChangesButton.setEnabled(False)
+        self.ui.cancelButton.setEnabled(False)
+        self.ui.createPackageButton.setEnabled(True)
+        self.ui.biPackageName.setEnabled(False)
+        self.ui.biPackageVersion.setEnabled(False)
+        self.ui.elementType.setCurrentIndex(0)
 
     def uploadPackageToGui(self):
-        for i in reversed(range(self.elements.count())):
-            self.elements.itemAt(i).widget().deleteLater()
+        for i in reversed(range(self.ui.elements.count())):
+            self.ui.elements.itemAt(i).widget().deleteLater()
         elements = self.biPackage.getElements()
         for i in range(len(elements)):
             (elementType, element), = elements[i].items()
-            self.elements.addWidget(QLabel(element.description), i, 0)
+            self.ui.elements.addWidget(QLabel(element.description), i, 0)
             btn = QPushButton('Remove')
             btn.clicked.connect(partial(self.biPackage.deleteElement, i))
             btn.clicked.connect(self.uploadPackageToGui)
-            self.elements.addWidget(btn, i, 1)
+            self.ui.elements.addWidget(btn, i, 1)
 
     @pyqtSlot(Element)
     def addElementGui(self, element):
         self.biPackage.addElement(element)
         self.uploadPackageToGui()
         self.packageElement.newElement.disconnect()
-        self.elementType.setCurrentIndex(0)
+        self.ui.elementType.setCurrentIndex(0)
 
 
 class LoadingScreen(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('forms/loadingscreen.ui', self)
+        self.ui = Ui_LoadScreenWindow()
+        self.ui.setupUi(self)
         self.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint |
                           Qt.WindowType.CustomizeWindowHint |
                           Qt.WindowType.FramelessWindowHint)

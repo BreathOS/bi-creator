f40fa4c540a3339e5635883a13f80d170359d936
Tolstosheyev Roman <izobretatel777@mail.ru>
2021-08-23 00:42:27 +0400

    replace usage of .ui with .py in controllers

--- controllers/PackageController.py
+++ controllers/PackageController.py
@@ -1,6 +1,6 @@
 import os
 
-from PyQt6 import uic
+from forms.package import Ui_AddPackageMenu
 from PyQt6.QtWidgets import QMessageBox, QFileDialog
 
 from controllers.ElementController import ElementController
@@ -10,40 +10,40 @@ from Elements import Package
 class PackageController(ElementController):
     def __init__(self):
         super().__init__()
-        uic.loadUi('forms/package.ui', self)
-        self.show()
-        self.isDeb.toggled.connect(self.setFile)
-        self.isApt.toggled.connect(self.setPackage)
-        self.chooseFile.clicked.connect(self.openFile)
-        self.saveButton.clicked.connect(self.saveElement)
-        self.cancelButton.clicked.connect(self.close)
+        self.ui = Ui_AddPackageMenu()
+        self.ui.setupUi(self)
+        self.ui.isDeb.toggled.connect(self.setFile)
+        self.ui.isApt.toggled.connect(self.setPackage)
+        self.ui.chooseFile.clicked.connect(self.openFile)
+        self.ui.saveButton.clicked.connect(self.saveElement)
+        self.ui.cancelButton.clicked.connect(self.close)
 
     def setFile(self):
-        self.aptPackageName.setEnabled(False)
-        self.chooseFile.setEnabled(True)
-        self.pathToDeb.setEnabled(True)
+        self.ui.aptPackageName.setEnabled(False)
+        self.ui.chooseFile.setEnabled(True)
+        self.ui.pathToDeb.setEnabled(True)
 
     def setPackage(self):
-        self.aptPackageName.setEnabled(True)
-        self.chooseFile.setEnabled(False)
-        self.pathToDeb.setEnabled(False)
+        self.ui.aptPackageName.setEnabled(True)
+        self.ui.chooseFile.setEnabled(False)
+        self.ui.pathToDeb.setEnabled(False)
 
     def openFile(self):
-        self.pathToDeb.setText(QFileDialog.getOpenFileName()[0])
+        self.ui.pathToDeb.setText(QFileDialog.getOpenFileName()[0])
 
     def saveElement(self):
-        if not self.isApt.isChecked() and not self.isDeb.isChecked():
+        if not self.ui.isApt.isChecked() and not self.ui.isDeb.isChecked():
             QMessageBox().critical(self, "Error!", "No package type selected!")
             return
-        if self.isApt.isChecked():
-            args = self.isRequired.isChecked(), self.aptPackageName.text(), 'apt', 'apt'
+        if self.ui.isApt.isChecked():
+            args = self.ui.isRequired.isChecked(), self.ui.aptPackageName.text(), 'apt', 'apt'
             if not self.isBlank(args):
                 self.newElement.emit(Package(*args))
                 self.close()
         else:
-            name = self.pathToDeb.text()[self.pathToDeb.text().rfind('/') + 1:]
-            src = self.pathToDeb.text()
-            args = self.isRequired.isChecked(), name, 'deb', src
+            name = self.ui.pathToDeb.text()[self.ui.pathToDeb.text().rfind('/') + 1:]
+            src = self.ui.pathToDeb.text()
+            args = self.ui.isRequired.isChecked(), name, 'deb', src
             if not os.path.isfile(src):
                 QMessageBox().critical(self, "Error! File does not exist", "File " + src + " does not exist!")
             elif not self.isBlank(args):
